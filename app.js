4de8579b97cfb3c33d283d7b039e050b1d698f7b
kalhollywood <kal.hollywood@gmail.com>
2022-08-19 11:28:23 +0100

    deploying to heroku

--- app.js
+++ app.js
@@ -12,10 +12,33 @@ app.get("/", function (req, res) {
   });
 });
 
-console.log(process.env.pgport);
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.use("/movies", moviesRouter);
+
 
 app.listen(PORT, function () {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// app.get("/api/search", async (req, res) => {
+//   try {
+//     const searchString = `q=${req.query.q}`;
+
+//     const response = await fetch(`https://www.goodreads.com/search/index.xml?key=${process.env.WEATHER_API_KEY}&${searchString}`);
+//     const xml = await response.text();
+
+//     const json = convert.xml2json(xml, { compact: true, spaces: 2 });
+
+//     const results = JSON.parse(json).GoodreadsResponse.search.results;
+
+//     return res.json({
+//       success: true,
+//       results
+//     })
+//   } catch (err) {
+//     return res.status(500).json({
+//       success: false,
+//       message: err.message,
+//     })
+//   }
+// })
